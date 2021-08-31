14a76ecb72b1700db70adfd1e5abfd333647071a
intom <iin.arahmah99@gmail.com>
2021-08-31 17:28:10 +0700

    file penulis: button tambah

--- penulis.php
+++ penulis.php
@@ -2,7 +2,19 @@
 include('layouts/header.php');
 include('layouts/navbar.php');
 
+// Tampil
 $query = mysqli_query($connect, 'SELECT * FROM penulis');
+
+//Tambah
+if (isset($_POST['submit'])) {
+    $penulis = $_POST['penulis'];
+    $email_penulis = $_POST['email_penulis'];
+    $alamat_penulis = $_POST['alamat_penulis'];
+    $sosmed_penulis = $_POST['sosmed_penulis'];
+
+    mysqli_query($connect, "INSERT INTO penulis(penulis, email_penulis, alamat_penulis, sosmed_penulis) VALUES ('$penulis', '$email_penulis', '$alamat_penulis','$sosmed_penulis')");
+    header('location:penulis.php');
+}
 ?>
 
 <!-- content penulis-->
@@ -13,7 +25,9 @@ $query = mysqli_query($connect, 'SELECT * FROM penulis');
             <div class="card white">
                 <div class="card-content black-text">
                     <span class="card-title">
-                        <a href="#" class="waves-effect waves-light cyan darken-4 btn-small" title="Add"><i class="material-icons left">add</i> Add</a>
+                        <!-- Modal Trigger -->
+                        <a class="waves-effect waves-light cyan darken-4 btn btn-small modal-trigger" href="#tambah"><i class="material-icons left">add</i> Tambah</a>
+
                     </span>
                     <table class="responsive-table">
                         <thead>
@@ -31,10 +45,10 @@ $query = mysqli_query($connect, 'SELECT * FROM penulis');
                             foreach ($query as $row) { ?>
                                 <tr>
                                     <td><?php echo $i++; ?></td>
-                                    <td><?php echo $row['nama'] ?></td>
-                                    <td><?php echo $row['email'] ?></td>
-                                    <td><?php echo $row['alamat'] ?></td>
-                                    <td><?php echo $row['sosmed'] ?></td>
+                                    <td><?php echo $row['penulis'] ?></td>
+                                    <td><?php echo $row['email_penulis'] ?></td>
+                                    <td><?php echo $row['alamat_penulis'] ?></td>
+                                    <td><?php echo $row['sosmed_penulis'] ?></td>
                                     <td>
                                         <a href="#" class="waves-effect waves-light light-blue darken-4 btn-small" title="Edit"><i class="material-icons">create</i></a>
                                         <a href="#" class="waves-effect waves-light red darken-4 btn-small" title="Delete"><i class="material-icons">delete</i></a>
@@ -49,8 +63,37 @@ $query = mysqli_query($connect, 'SELECT * FROM penulis');
     </div>
 </div>
 
+<!-- Modal Tambah Data-->
+<div id="tambah" class="modal">
+    <div class="modal-content">
+        <div class="row">
+            <h4>Form Penulis</h4>
+            <form action="penulis.php" method="POST">
+                <div class="input-field col s12 m12">
+                    <input id="penulis" name="penulis" type="text" class="validate">
+                    <label for="penulis">Nama Penulis</label>
+                </div>
+                <div class="input-field col s12 m12">
+                    <input id="email_penulis" name="email_penulis" type="email" class="validate">
+                    <label for="email_penulis">Email</label>
+                </div>
+                <div class="input-field col s12 m12">
+                    <input id="sosmed_penulis" name="sosmed_penulis" type="text" class="validate">
+                    <label for="sosmed_penulis">Sosial Media</label>
+                </div>
+                <div class="input-field col s12 m12">
+                    <textarea id="alamat_penulis" name="alamat_penulis" class="materialize-textarea"></textarea>
+                    <label for="alamat_penulis">Alamat</label>
+                </div>
 
-
+        </div>
+    </div>
+    <div class="modal-footer">
+        <a href="#" class="modal-close waves-effect waves-light grey btn-small" title="Batal"><i class="material-icons left">cancel</i> Batal</a>
+        <button type="submit" name="submit" title="Simpan" class="waves-effect waves-light light-blue darken-4 btn-small"><i class="material-icons left">save</i> Simpan</button>
+        </form>
+    </div>
+</div>
 
 <?php
 include('layouts/footer.php');
